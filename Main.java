44d4629760279e8d01b45f8797b019ec2a32da19
hashimoto-hayato <e1n22054@oit.ac.jp>
2025-06-22 16:22:53 +0900

    v2.0: プレイヤー機能を追加

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    System.out.println("SEMonster!");
+    Player Player = new Player();
+    player.drawMonsters();
+    player.showDedk();
   }
 }
